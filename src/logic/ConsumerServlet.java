2cff8600a3afac86bb7e6d2756945f5adfa4bbf5
Cottonbuds <mchan@Michaels-MacBook-Pro.local>
2014-05-16 19:32:18 +1000

    added starting bits to consumer servlet

--- src/logic/ConsumerServlet.java
+++ src/logic/ConsumerServlet.java
@@ -1,11 +1,18 @@
 package logic;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Exception.InvalidActionException;
+import jdbc.DAO;
 
 /**
  * Servlet implementation class ConsumerServlet
@@ -13,13 +20,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ConsumerServlet")
 public class ConsumerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    private DAO dao;
+    private Connection connection;
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ConsumerServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        dao = new DAO();
+        
     }
 
 	/**
@@ -27,6 +37,7 @@ public class ConsumerServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		doPost(request, response);
 	}
 
 	/**
@@ -34,6 +45,53 @@ public class ConsumerServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String forwardPage = "";
+		String action = request.getParameter("action");
+		
+		if(action.equals(null)){
+			try {
+				throw new InvalidActionException();
+			} catch (InvalidActionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(action.equals("search")){
+			String checkindate = request.getParameter("startday");
+			String checkinmonth = request.getParameter("startmonth");
+			String checkinyear = request.getParameter("startyear");
+			
+			String checkoutdate = request.getParameter("endday");
+			String checkoutmonth = request.getParameter("endmonth");
+			String checkoutyear = request.getParameter("endyear");
+			
+			String city = request.getParameter("cityChosen");
+			
+			String maxprice = request.getParameter("maxprice");
+			
+			String checkinsqldate = checkindate+"-"+checkinmonth+"-"+checkinyear;
+			String checkoutsqldate = checkoutdate+"-"+checkoutmonth+"-"+checkoutyear;
+			
+			//needs a select statement here
+			
+		}
+		if(action.equals("confirm")){
+			
+			
+		}
+		if(action.equals("checkout")){
+			
+			
+		}
+		if(action.equals("complete")){
+			
+			
+		}
+		if(action.equals("reset")){
+			
+			
+		}
+		
 	}
 
 }
