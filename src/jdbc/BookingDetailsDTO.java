40eaaff47a533c35f732133b68b6993fbf55bd7c
Cottonbuds <mchan@Michaels-MacBook-Pro.local>
2014-05-18 13:26:02 +1000

    fixed merge conflicts on DAO

---
+++ src/jdbc/BookingDetailsDTO.java
@@ -0,0 +1,62 @@
+package jdbc;
+
+import java.sql.Date;
+
+public class BookingDetailsDTO {
+private String hotelname;
+private String roomtype;
+private Date startdate;
+private Date enddate;
+private String firstname;
+private String lastname;
+
+
+public BookingDetailsDTO(String hotelname, String roomtype, Date startdate, Date enddate, String firstname, String lastname){
+	setHotelname(hotelname);
+	setRoomtype(roomtype);
+	setStartdate(startdate);
+	setEnddate(enddate);
+	setFirstname(firstname);
+	setLastname(lastname);
+}
+
+public String getHotelname() {
+	return hotelname;
+}
+public void setHotelname(String hotelname) {
+	this.hotelname = hotelname;
+}
+public String getRoomtype() {
+	return roomtype;
+}
+public void setRoomtype(String roomtype) {
+	this.roomtype = roomtype;
+}
+public Date getStartdate() {
+	return startdate;
+}
+public void setStartdate(Date startdate) {
+	this.startdate = startdate;
+}
+public Date getEnddate() {
+	return enddate;
+}
+public void setEnddate(Date enddate) {
+	this.enddate = enddate;
+}
+public String getFirstname() {
+	return firstname;
+}
+public void setFirstname(String firstname) {
+	this.firstname = firstname;
+}
+public String getLastname() {
+	return lastname;
+}
+public void setLastname(String lastname) {
+	this.lastname = lastname;
+}
+
+	
+	
+}
